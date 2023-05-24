dce09d6a0a48dc6f1367ec44df49b4cda339cef0
Xia Jinyi <jinyi.xia@bupt.edu.cn>
2023-05-24 09:22:49 +0800

    Update main.cpp

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 #include "BasicTcp.h"
+#include "Email.h"
+
+using namespace MailProxy;
 
 void print_log(const char* log) {
-    std::cout << log << std::endl;
+    std::cout << "Tcp::" << log << std::endl;
 }
 
+/**
+ * @brief 发送邮件（未进行错误处理）。
+ * @return 返回值
+*/
 int main() {
-    MailProxy::BasicTcp tcp_conn(4096, print_log);
-    tcp_conn.connect("127.0.0.1", "80");
-    tcp_conn.send(NULL);
-    tcp_conn.receive();
-    tcp_conn.shutdown();
+    ClientTcp tcp_conn(8192, print_log);
+    tcp_conn.tcp_connect("smtp.126.com", "25"); // 若服务器为 QQ 邮箱，则发送消息的结尾应为 "\r\n"
+    tcp_conn.tcp_receive();
+    std::cout << tcp_conn.get_received() << std::endl;
+
+    tcp_conn.tcp_send("EHLO Bupt\n");
+    tcp_conn.tcp_receive();
+    std::cout << tcp_conn.get_received() << std::endl;
+
+    // tcp_conn.tcp_send("AUTH LOGIN\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+    // tcp_conn.tcp_send("eGlhamlueWlAMTI2LmNvbQ==\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+    // tcp_conn.tcp_send("********PASSWORD********\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+
+    // tcp_conn.tcp_send("MAIL FROM:<xiajinyi@126.com>\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+    // tcp_conn.tcp_send("RCPT TO:<jinyi.xia@bupt.edu.cn>\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+
+    // tcp_conn.tcp_send("DATA\n");
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+    // tcp_conn.tcp_send(
+    //     "From: \"=?utf-8?B?eGlhamlueWk=?=\"<xiajinyi@126.com>\n"
+    //     "To: \"=?utf-8?B?amlueWkueGlh?=\"<jinyi.xia@bupt.edu.cn>\n"
+    //     "Subject: Test tcp client\n"
+    //     "\n"
+    //     "A quick brown fox jumps over the lazy dog.\n"
+    //     "\n.\n"
+    // );
+    // tcp_conn.tcp_receive();
+    // std::cout << tcp_conn.get_received() << std::endl;
+    
+    tcp_conn.tcp_send("QUIT\n");
+    tcp_conn.tcp_receive();
+    std::cout << tcp_conn.get_received() << std::endl;
+
+    // 一般情况下不需要手动 shutdown
+    tcp_conn.tcp_shutdown();
     return 0;
 }
